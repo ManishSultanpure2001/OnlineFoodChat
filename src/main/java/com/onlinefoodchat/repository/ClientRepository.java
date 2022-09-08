04d0aded185046819707786c69c89d5ba4a537db
ManishSultanpure2001 <manish.s@webkorps.com>
2022-09-09 03:31:13 +0530

    Did the work on updation and deletion of manu

--- src/main/java/com/onlinefoodchat/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.onlinefoodchat.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.onlinefoodchat.entity.ClientLogin;
-
-public interface ClientRepository extends JpaRepository<ClientLogin, Integer>{
-		
-}
